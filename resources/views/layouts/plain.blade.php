b59a4ce651820cd6ed3c840ffa1a50463f8e264d
Nur Bar <nbfbce@ymail.com>
2017-01-24 10:50:44 -0300

    Added

---
+++ resources/views/layouts/plain.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+		window.Laravel = <?= json_encode([
+			'csrfToken' => csrf_token(),
+		]); ?>
+    </script>
+
+    @yield('head')
+</head>
+<body>
+<div id="app">
+    @yield('content')
+
+
+    @yield('scripts')
+</div>
+
+<!-- Scripts -->
+<script src="/js/app.js"></script>
+</body>
+</html>
