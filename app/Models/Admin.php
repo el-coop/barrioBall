7a879d8beef524ee775108122490ba2a01889928
aliciaderuijter <aliciaderuijter@gmail.com>
2017-02-02 17:04:37 -0300

    Added admin and regular user. first user is admin.

---
+++ app/Models/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    public function user()
+    {
+        return $this->morphOne(User::class, 'user');
+    }
+}
--- database/migrations/2017_02_01_231409_create_regulars_table.php
+++ database/migrations/2017_02_01_231409_create_regulars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRegularsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('regulars', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('language');
-            $table->integer('user_id');
-            $table->string('user_type');
-            $table->rememberToken();
             $table->timestamps();
-
         });
     }
 
@@ -33,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('regulars');
     }
 }
