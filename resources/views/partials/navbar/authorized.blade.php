76fddafc260c614f282c7d3d2ce4a4a4f68f10f6
Nur Bar <nbfbce@ymail.com>
2017-05-03 23:34:05 -0500

    updated bootstrap version. all pages need to be redone!

--- resources/views/partials/navbar/authorized.blade.php
+++ resources/views/partials/navbar/authorized.blade.php
@@ -1,26 +1,29 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+    <div class="container mr-0 ml-0 mr-md-auto ml-md-auto sm-position-static">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown">
+                        {{ config('languages')[App::getLocale()] }}
+                        <b class="caret"></b>
+                    </a>
+                    <div class="dropdown-menu">
+                        @foreach (config('languages') as $lang => $language)
+                            <a href="{{action ('LanguageController@switchLang', $lang) }}"
+                               class="dropdown-item">{{$language}}</a>
+                        @endforeach
+                    </div>
+                </li>
+            </ul>
         </div>
-        <ul class="nav navbar-nav navbar-right hidden-sm hidden-xs">
-            <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    {{ config('languages')[App::getLocale()] }}
-
-                    <b class="caret"></b></a>
-                <ul class="dropdown-menu" role="menu">
-                    @foreach (config('languages') as $lang => $language)
-                        @if ($lang != App::getLocale())
-                            <li>
-                                <a href="{{action ('LanguageController@switchLang', $lang) }}">{{$language}}</a>
-                            </li>
-                        @endif
-                    @endforeach
-                </ul>
-            </li>
-        </ul>
     </div>
-
 </nav>
-
