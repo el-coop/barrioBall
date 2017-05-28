532486d898164657f2a0ab09a811b0cec3f04c01
Nur Bar <nbfbce@ymail.com>
2017-05-29 00:09:05 +0300

    Moved search translation into a folder

--- resources/views/match/search.blade.php
+++ resources/views/match/search.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     @include('partials.navbar.authorized')
 
-    <search-page inline-template map-name="@lang('search.map')" search-name="@lang('search.search')">
+    <search-page inline-template map-name="@lang('match/search.map')" search-name="@lang('match/search.search')">
         <div class="container-fluid sm-full-height"
              :class="{ 'sm-no-side-padding' : mapToggled, 'fixed-loading' : loading}" id="search-page">
             <flipper :flipped="mapToggled">
@@ -19,21 +19,21 @@
                                 <div class="mb-2 mr-sm-2 w-100-sm-down align-self-baseline"
                                      :class="{'has-danger' : errors.hasOwnProperty('date')}"
                                      id="date_group">
-                                    <date-picker label="@lang('search.date'):" name="date"></date-picker>
+                                    <date-picker label="@lang('match/search.date'):" name="date"></date-picker>
                                     <span class="form-control-feedback"
                                           v-if="errors.hasOwnProperty('date')">* @{{ errors.date[0] }}</span>
                                 </div>
                                 <div class="mb-2 mr-sm-2 w-100-sm-down align-self-baseline"
                                      :class="{'has-danger' : errors.hasOwnProperty('from')}"
                                      id="start_time_group">
-                                    <time-picker label="@lang('search.from'):" name="from"></time-picker>
+                                    <time-picker label="@lang('match/search.from'):" name="from"></time-picker>
                                     <span class="form-control-feedback"
                                           v-if="errors.hasOwnProperty('from')">* @{{ errors.from[0] }}</span>
                                 </div>
                                 <div class="mb-2 mr-sm-2 w-100-sm-down align-self-baseline"
                                      :class="{'has-danger' : errors.hasOwnProperty('to')}"
                                      id="end_time_group">
-                                    <time-picker label="@lang('search.to'):" name="to"></time-picker>
+                                    <time-picker label="@lang('match/search.to'):" name="to"></time-picker>
                                     <span class="form-control-feedback"
                                           v-if="errors.hasOwnProperty('to')">* @{{ errors.to[0]}}</span>
                                 </div>
@@ -44,7 +44,7 @@
                     <div class="row">
                         <div class="col-12" v-if="matches != null && !matches.length">
                             <h4 class="text-center">
-                                @lang('search.noMatchesFound')
+                                @lang('match/search.noMatchesFound')
                             </h4>
                         </div>
                         <div class="col-12 col-md-6 mb-3" v-for="(match, index) in matches">
@@ -62,7 +62,7 @@
                                         <div class="row">
                                             <div class="col-7"><strong>@{{ match.address }}</strong></div>
                                             <div class="col-5 text-right">
-                                                <strong>@{{ match.players }} @lang('search.players')</strong></div>
+                                                <strong>@{{ match.players }} @lang('match/search.players')</strong></div>
                                             <div class="col-12">
                                                 <p>@{{ match.description }}</p>
                                             </div>
@@ -77,8 +77,8 @@
                             <paginate
                                     :click-handler="getPage"
                                     :page-count="pages"
-                                    prev-text="@lang('search.prev')"
-                                    next-text="@lang('search.next')"
+                                    prev-text="@lang('match/search.prev')"
+                                    next-text="@lang('match/search.next')"
                                     container-class="pagination mx-auto"
                                     prev-class="page-item"
                                     prev-link-class="page-link"
