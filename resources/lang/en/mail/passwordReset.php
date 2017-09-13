d2902a6fb47a8d4e1ea55d6561c8aeae2692c340
Nur Bar <nbfbce@ymail.com>
2017-09-12 23:06:57 -0300

    Translated password notification

---
+++ resources/lang/en/mail/passwordReset.php
@@ -0,0 +1,7 @@
+<?php
+return [
+	'subject' => 'Reset Password',
+	'firstLine' => 'You are receiving this email because we received a password reset request for your account.',
+	'button' => 'Reset Password',
+	'lastLine' => 'If you did not request a password reset, no further action is required.'
+];
