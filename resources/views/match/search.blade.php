a33a853d3bb0a598774756e0756bdc4e90f6ec48
Nur Bar <nbfbce@ymail.com>
2017-09-12 23:45:53 -0300

    fixed email shape

--- resources/views/match/search.blade.php
+++ resources/views/match/search.blade.php
@@ -21,24 +21,24 @@
                                        btn-wrapper-class="sm-btn-block align-self-baseline"
                                        ref="form">
                                 <div class="mb-2 mr-sm-2 w-100-sm-down align-self-baseline"
-                                     :class="{'has-danger' : errors.hasOwnProperty('date')}"
+                                     :class="{'is-invalid' : errors.hasOwnProperty('date')}"
                                      id="date_group">
                                     <date-picker label="@lang('match/search.date'):" name="date"></date-picker>
-                                    <span class="form-control-feedback"
-                                          v-if="errors.hasOwnProperty('date')">* @{{ errors.date[0] }}</span>
+                                    <span class="invalid-feedback"
+                                          v-if="errors.hasOwnProperty('to')">* @{{ errors.date[0]}}</span>
                                 </div>
                                 <div class="mb-2 mr-sm-2 w-100-sm-down align-self-baseline"
-                                     :class="{'has-danger' : errors.hasOwnProperty('from')}"
+                                     :class="{'is-invalid' : errors.hasOwnProperty('from')}"
                                      id="start_time_group">
                                     <time-picker label="@lang('match/search.from'):" name="from"></time-picker>
-                                    <span class="form-control-feedback"
+                                    <span class="invalid-feedback"
                                           v-if="errors.hasOwnProperty('from')">* @{{ errors.from[0] }}</span>
                                 </div>
                                 <div class="mb-2 mr-sm-2 w-100-sm-down align-self-baseline"
-                                     :class="{'has-danger' : errors.hasOwnProperty('to')}"
+                                     :class="{'is-invalid' : errors.hasOwnProperty('to')}"
                                      id="end_time_group">
                                     <time-picker label="@lang('match/search.to'):" name="to"></time-picker>
-                                    <span class="form-control-feedback"
+                                    <span class="invalid-feedback"
                                           v-if="errors.hasOwnProperty('to')">* @{{ errors.to[0]}}</span>
                                 </div>
                                 <i class="fa fa-search" slot="submit"></i>
