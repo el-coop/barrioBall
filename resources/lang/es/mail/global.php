fd9a7042868ccba3261717be989cbbb983a2a9d8
Nur Bar <nbfbce@ymail.com>
2017-12-11 14:37:58 -0200

    fixed a type in spanish

--- resources/lang/es/mail/global.php
+++ resources/lang/es/mail/global.php
@@ -2,7 +2,7 @@
 return [
 	'hello' => 'Hola',
 	'adminSays' => 'El administrador dice:',
-	'dontReply' => 'Por favor nao responda a este mensaje',
+	'dontReply' => 'Por favor no responda a este mensaje',
 	'replyToThisEmail' => 'Puede responder a este mensaje respondiendo a este correo electronico.',
 	'buttonProblems' => "Si tiene problemas al hacer clic en el botón, copie y pegue la siguiente URL en su navegador web"
 ];
