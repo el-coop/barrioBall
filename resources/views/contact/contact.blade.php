eea2c34395a1f61afdaaf0c7dd7d2394d7ef16e1
Nur Bar <nbfbce@ymail.com>
2017-12-11 15:23:47 -0200

    page layout

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -9,6 +9,14 @@
             <h1>@lang('navbar.contactLink')</h1>
         </div>
     </div>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-12 col-md-7">
+                <input class="form-control">
+            </div>
+            <div class="col-12 col-md-3">Test</div>
+        </div>
+    </div>
 
     <div class="footer py-2">
         <div class="container">
