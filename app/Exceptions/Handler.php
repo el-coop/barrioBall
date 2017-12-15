5e26e1fdb8f09778238d116b208f6b913de8574a
Nur Bar <nbfbce@ymail.com>
2017-12-15 12:19:18 -0200

    rebased

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -54,7 +54,6 @@ class Handler extends ExceptionHandler {
 	 * @param Exception $exception
 	 */
 	protected function LogException(Exception $exception): void {
-		dump($exception);
 		$request = request();
 		$error = new Error;
 		$phpError = new PhpError;
