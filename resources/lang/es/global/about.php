4a208328d92cb1c04e86f046af94e7db93b6d994
Cucurrucho <adam.bar91@gmail.com>
2017-12-23 11:15:56 -0300

    translated about page

---
+++ resources/lang/es/global/about.php
@@ -0,0 +1,15 @@
+<?php
+return [
+
+    'barrioBallText' => 'Barrio-Ball es una herramienta para el fútbol barrial, nuestro objetivo es conectar a gente que quiere jugar, y grupos que les faltan jugadores',
+    'coopTitle' => 'La COOP Del Nuevo Día',
+    'coopButton' => 'El Equipo',
+    'coopText' => 'Somos programadores trabajando juntos, con igualdad de salario, dividiendo la carga horaria según la necesidad y las tareas según la capacidad, ayudando a los novicios avanzar aprendiendo de los expertos y uno del otro',
+    'privacyTitle' => 'Privacidad',
+    'privacyText' => 'Creemos en la privacidad de todos nuestros usuarios. Solamente guardamos los datos necesarios para el funcionamiento, además evitamor usar servicios que rastrean datos de nuestros usuarios',
+    'openSourceTitle' => 'Código Abierto',
+    'openSourceText' => 'Todo nuestro código es abierto bajo licencia GPL-3.0. Creemos que el código provee de oportunidad de aprender y compartir el conocimiento',
+    'footballTitle' => 'Fútbol',
+    'footballText' => '<blockquote class="blockquote"><p class="mb-0">La pelota no se mancha</p><footer class="blockquote-footer">Diego Armando Maradona</footer></blockquote><p>Sin importar su nivel, sin importar si se les paga por hacerlo, cada dia millones de personas lo sienten</p>'
+
+];
