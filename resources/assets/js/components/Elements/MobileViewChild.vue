43ca24f2f574b2d0822c50b319bba80d3f48fbf7
Nur Bar <nbfbce@ymail.com>
2017-12-28 20:55:38 -0200

    added responsive view switch

---
+++ resources/assets/js/components/Elements/MobileViewChild.vue
@@ -0,0 +1,40 @@
+<template>
+    <transition name="switch-fade">
+        <div v-show="isActive">
+            <slot></slot>
+        </div>
+    </transition>
+</template>
+<script>
+	export default {
+		props: {
+			icon: {
+				type: String,
+				required: true
+			},
+		},
+
+		data() {
+			return {
+				active: false
+			}
+		},
+
+        computed: {
+		    isActive(){
+		    	return this.active || this.$mq == 'lg' || this.$mq == 'xl';
+            }
+        },
+
+		methods: {
+			toggle(active) {
+				if (active == this._uid) {
+					this.active = true;
+				} else {
+					this.active = false;
+				}
+			}
+		}
+	}
+</script>
+
---
+++ resources/assets/js/components/Elements/MobileViewParent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mobile-view">
+        <div class="bubble-wrapper d-lg-none">
+            <div v-for="child in children" key="child._uid" v-show="child._uid != active"
+                 class="btn btn-primary bubble" @click="activate(child)">
+                <i class="fa m-auto fa-2x" :class="child.icon"></i>
+            </div>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				children: [],
+				active: null
+			}
+		},
+
+		methods: {
+			activate(child) {
+				this.children.forEach((element) => {
+					element.toggle(child._uid);
+				});
+				this.active = child._uid;
+			}
+		},
+
+		mounted() {
+			this.children = this.$children;
+			this.activate(this.children[0]);
+		}
+	}
+</script>
+
+
+
